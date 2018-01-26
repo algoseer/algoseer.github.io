99b936794965eb94f09b5e817094fe1c1a5c9279
algoseer <knaveen87@gmail.com>
2018-01-26 11:42:30 -0800

    Initial commit

---
+++ research/freemind/map.mm
@@ -0,0 +1,109 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1342305594345" ID="ID_1252315299" MODIFIED="1344027663633" TEXT="SAIL">
+<font BOLD="true" NAME="SansSerif" SIZE="23"/>
+<node CREATED="1342305837385" FOLDED="true" HGAP="46" ID="ID_1344618779" MODIFIED="1358723012770" POSITION="right" TEXT="Research" VSHIFT="10">
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node CREATED="1342817532876" FOLDED="true" ID="ID_1391838075" MODIFIED="1344027671661" TEXT="Audio Search">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+<node CREATED="1344026293689" ID="ID_1771449018" MODIFIED="1344026304469" TEXT="Cross lingual tune matching"/>
+<node CREATED="1344026320810" ID="ID_811589724" MODIFIED="1344026328310" TEXT="General audio searching"/>
+</node>
+<node CREATED="1342306576676" FOLDED="true" ID="ID_953207478" MODIFIED="1358476932388" TEXT="Feature Selection">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1344026612472" ID="ID_1462968663" MODIFIED="1344026631396" TEXT="Feature grouping for &#xa;feature selection"/>
+<node CREATED="1344026634688" ID="ID_1212636255" MODIFIED="1344026639757" TEXT="Outlier feature detection"/>
+</node>
+<node CREATED="1342312027835" FOLDED="true" ID="ID_1442064567" MODIFIED="1358476931972" TEXT="Speech Entropy">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1344026669689" ID="ID_154220871" MODIFIED="1344026723854" TEXT="Dependence between &#xa;speech and text within &#xa;and across languages"/>
+</node>
+<node CREATED="1358476811256" FOLDED="true" ID="ID_294914579" MODIFIED="1358476930775" TEXT="Subclustering">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1358476817632" ID="ID_734763857" MODIFIED="1358476834780" TEXT="Do finer labels help &#xa;in classification?"/>
+<node CREATED="1358476836761" ID="ID_1357084980" MODIFIED="1358476927000" TEXT="Data driven techniques&#xa;to infer finer labels&#xa;from coarse labels."/>
+</node>
+</node>
+<node CREATED="1342646675342" FOLDED="true" HGAP="-22" ID="ID_1043030375" MODIFIED="1358477001316" POSITION="left" TEXT="Courses" VSHIFT="-87">
+<font BOLD="true" NAME="SansSerif" SIZE="19"/>
+<node CREATED="1343535376270" FOLDED="true" HGAP="81" ID="ID_1901206195" MODIFIED="1344025935668" TEXT="EE" VSHIFT="-9">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1343534854411" ID="ID_1908415515" MODIFIED="1343699496789" TEXT="Probability and Statistics">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1343534872388" ID="ID_1688580323" MODIFIED="1343699497012" TEXT="Random Processes">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1343535159425" ID="ID_1418211377" MODIFIED="1343699634640" TEXT="Wavelets">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1343535281868" FOLDED="true" HGAP="87" ID="ID_745508150" MODIFIED="1344025940388" TEXT="CS" VSHIFT="-3">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1343534877115" ID="ID_110622639" MODIFIED="1343534879375" TEXT="Computer Vision"/>
+<node CREATED="1343535138489" ID="ID_72884960" MODIFIED="1343699630916" TEXT="Empirical NLP"/>
+</node>
+<node CREATED="1343534984350" FOLDED="true" HGAP="51" ID="ID_1195836037" MODIFIED="1344025939899" TEXT="Signal &#xa;Processing" VSHIFT="-7">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1343535020422" ID="ID_1969665936" MODIFIED="1343535026074" TEXT="Image Processing"/>
+<node CREATED="1343535027302" ID="ID_299074885" MODIFIED="1343535339584" TEXT="Speech Recognition"/>
+</node>
+<node CREATED="1343534924278" FOLDED="true" HGAP="52" ID="ID_84300621" MODIFIED="1344025939403" TEXT="Machine &#xa;Learning" VSHIFT="-16">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1343534893884" ID="ID_1652777983" MODIFIED="1343534904416" TEXT="Machine Learning"/>
+<node CREATED="1343534880140" ID="ID_322363524" MODIFIED="1343534892167" TEXT="Pattern Recognition"/>
+<node CREATED="1343534905012" ID="ID_1966908852" MODIFIED="1343534916672" TEXT="Probabilistic Graphical Models"/>
+</node>
+<node CREATED="1358476618956" FOLDED="true" ID="ID_840383666" MODIFIED="1358476632298" TEXT="Maths">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1358476625114" ID="ID_1031140011" MODIFIED="1358476631306" TEXT="Optimization Theory"/>
+</node>
+</node>
+<node CREATED="1342305864648" FOLDED="true" HGAP="40" ID="ID_921968998" MODIFIED="1358723011386" POSITION="left" TEXT="Projects" VSHIFT="25">
+<font BOLD="true" NAME="SansSerif" SIZE="19"/>
+<node CREATED="1342311027927" FOLDED="true" ID="ID_1020161343" MODIFIED="1358476938148" STYLE="fork" TEXT="ASR">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1343535895616" FOLDED="true" ID="ID_1851772448" MODIFIED="1344025858697" TEXT="Front end development&#xa;for the SAIL ASR">
+<node CREATED="1344025837736" ID="ID_328553646" MODIFIED="1344025840100" TEXT="Denoising"/>
+<node CREATED="1344025842352" ID="ID_83182523" MODIFIED="1344025849468" TEXT="Feature transformation"/>
+</node>
+<node CREATED="1358476639547" ID="ID_1024032073" MODIFIED="1358476649358" TEXT="Denoising module for SAIL-KALDI"/>
+</node>
+<node CREATED="1342310987397" FOLDED="true" ID="ID_171550482" MODIFIED="1358476939550" TEXT="Be A Scientist">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1343535814326" FOLDED="true" ID="ID_1989082440" MODIFIED="1358476659171" TEXT="Behavior assessment&#xa;in a classrooom scenario">
+<node CREATED="1344025770799" ID="ID_1517540918" MODIFIED="1344025782476" TEXT="Engagement / disengagement"/>
+<node CREATED="1344025783615" ID="ID_551194381" MODIFIED="1344026080817" TEXT="Dealing with lack of &#xa;or incomplete annotations"/>
+</node>
+</node>
+<node CREATED="1342308121614" FOLDED="true" ID="ID_178703802" MODIFIED="1358476941007" TEXT="Underwater Acoustics">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+<node CREATED="1342308126045" FOLDED="true" ID="ID_43603043" MODIFIED="1358476702509" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Robust Object<br />Classification in<br />Underwater Sonar
+    </p>
+    <p>
+      Images
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<node CREATED="1344025626588" ID="ID_1823907614" MODIFIED="1344025633152" TEXT="Image Segmentation"/>
+<node CREATED="1344025634276" ID="ID_1201425341" MODIFIED="1344025639536" TEXT="Feature Representation"/>
+<node CREATED="1344025640292" ID="ID_1522453668" MODIFIED="1344025691089" TEXT="Sparse reconstruction &#xa;based classification"/>
+</node>
+<node CREATED="1358476673235" ID="ID_500941323" MODIFIED="1358476697624" TEXT="Reliability Aware combination&#xa;of different feature sets&#xa;for object classification"/>
+<node CREATED="1358476704060" ID="ID_1405451727" MODIFIED="1358476719785" TEXT="Adapative Random Access &#xa;Compress Sensing"/>
+</node>
+<node CREATED="1358476744118" FOLDED="true" ID="ID_766642005" MODIFIED="1358476961637" TEXT="Pathology">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1358476752544" ID="ID_470544895" MODIFIED="1358476960329" TEXT="Assessing speech&#xa;intelligibility from speech &#xa;and articulatory signal"/>
+</node>
+</node>
+</node>
+</map>
